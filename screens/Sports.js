3cb0c75e281bbe1ee34d958423b8d9dbee9ec70b
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-02-09 20:24:36 +0530

    feat: adding ui of app

--- screens/Sports.js
+++ screens/Sports.js
@@ -1,4 +1,5 @@
 import { View, Text, StyleSheet } from 'react-native';
+import { Divider, NativeBaseProvider } from 'native-base';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,14 +13,62 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  flex: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+  },
+  date: {
+    fontSize: 20,
+  },
+  description: {
+    padding: 20,
+  },
 });
+
 const SportsScreen = () => {
   return (
-    <View>
-      <View style={styles.container}>
-        <Text style={styles.text}>Sports news</Text>
+    <NativeBaseProvider>
+      <View>
+        <View style={styles.container}>
+          <Text style={styles.text}>Sports News</Text>
+        </View>
+        <View>
+          <View style={styles.flex}>
+            <Text style={styles.title}>Title</Text>
+            <Text style={styles.date}>Date</Text>
+          </View>
+          <View style={styles.description}>
+            <Text style={styles.title}>Description</Text>
+          </View>
+        </View>
+        <Divider my={2} bg="purple.500" />
+        <View>
+          <View style={styles.flex}>
+            <Text style={styles.title}>Title</Text>
+            <Text style={styles.date}>Date</Text>
+          </View>
+          <View style={styles.description}>
+            <Text style={styles.title}>Description</Text>
+          </View>
+        </View>
+        <Divider my={2} bg="purple.500" />
+        <View>
+          <View style={styles.flex}>
+            <Text style={styles.title}>Title</Text>
+            <Text style={styles.date}>Date</Text>
+          </View>
+          <View style={styles.description}>
+            <Text style={styles.title}>Description</Text>
+          </View>
+        </View>
+        <Divider my={2} bg="purple.500" />
       </View>
-    </View>
+    </NativeBaseProvider>
   );
 };
 
